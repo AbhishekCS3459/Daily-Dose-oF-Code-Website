9fad71fae4ff1673969f87286ffa0f159c654de4
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2022-11-24 06:07:49 +0530

    weebsite template

---
+++ index.js
@@ -0,0 +1,77 @@
+function changePicture(){
+    var yourImages = ["coding logo_adobe_express.svg","IMG_20220705_141104.jpg"];
+    var randomImage = Math.round(Math.random()*yourImages.length);
+    var setImg = document.getElementById("currentImage").src;
+    setImg = yourImages[randomImage];
+   }
+ 
+
+
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/dailydoseofcode/youtube/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
+
+
+$(document).ready(function(){
+  
+   
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["an Intern", "a Developer",  "a Web Designer", "a Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["an Intern", "a Developer", "a Website Designer", "a Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
